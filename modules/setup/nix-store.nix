e1861c3f7e442f2d07afd4c34166bec269c9c97e
Matthew B. Reisch <dev@tracerte.com>
2020-10-19 17:27:30 -0400

    Made changes to Nix Store GC to preserve nix-shell

--- modules/setup/nix-store.nix
+++ modules/setup/nix-store.nix
@@ -9,6 +9,11 @@ in
   };
 
   config = mkIf cfg.enable {
+    # Protect nix-shell from GC
+    nix.extraOptions = ''
+      keep-outputs = true
+      keep-derivations = true
+      '';
     # Nix Store Maintenance
     nix.autoOptimiseStore = true;
     # Use gc.automatic to keep disk space under control.
